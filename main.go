7cbb60a22915ef2e49a8d3c22da483fb59650986
kyusufm <kyusufm@gmail.com>
2022-03-31 08:56:32 +0700

    learned, variable, default variable, if, switch case equal, switch case with case

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"apertama/calculation"
-	"apertama/multiply"
+	//"apertama/calculation"
+	//"apertama/multiply"
 	"fmt"
 )
 
@@ -11,9 +11,49 @@ func main() {
 	//sentence := testAja()
 	//fmt.Println(sentence)
 
-	result := multiply.Mult(8, 9)
-	resultTambah := calculation.Add(8, 9)
+	//result := multiply.Mult(8, 9)
+	//resultTambah := calculation.Add(8, 9)
 
-	fmt.Println(result, resultTambah)
+	//fmt.Println(result, resultTambah)
 
+	//var name string = "Golang"
+
+	//var name string
+	//name = "joki"
+	//fmt.Println(name)
+
+	// age := 21
+
+	// if age > 20 {
+	// 	fmt.Println("sudah dewasa?")
+	// } else if age >= 10 {
+	// 	fmt.Println("wah udah besar")
+	// } else {
+	// 	fmt.Println("wah masih kecil")
+	// }
+
+	//case
+	numero := 2
+
+	switch numero {
+	case 1:
+		fmt.Println("satu")
+	case 2:
+		fmt.Println("dua")
+	case 3:
+		fmt.Println("tiga")
+	default:
+		fmt.Println("tidak terpenuhi")
+	}
+
+	switch {
+	case numero > 8:
+		fmt.Println("kenceng nih")
+	case numero > 4:
+		fmt.Println("santai bos")
+	case numero > 1:
+		fmt.Println("pelan pelan")
+	default:
+		fmt.Println("jangan malas")
+	}
 }
--- notes.md
+++ notes.md
@@ -27,3 +27,31 @@ library bisa selain main.
 
 penamaan fungsi di library, harus diawali huruf besar agar bisa diakses dari main.
 jika mengawali dengan huruf kecil tidak bisa diakses dari fungsi main, tapi bisa diakses oleh fungsi lain di dalam file tersebut.
+
+
+menggunakan import.
+2.memanggil library dari package berbeda yang kita buat.
+3.memanggil package lain yang dibikin orang lain.
+1.memanggil default library dari golang (misal fmt.)
+
+func main
+wajib, karena ini fungsi utama yang akan di execute.
+
+Variable, tipe data, dan default value
+kenapa harus mendefenisikan tipe data ?
+karena golang termasuk static type (serperti java, c#)
+
+tipe umum- string, int, float64, bool
+
+kalau ingin membuat variable tanpa harus mengisi typenya atau mengetik var, kita bisa mengetik nama :=
+
+tidak bisa di bikin 2 kali, misal age:=20, kemudian diketik lagi age:=30 tidak bisa. karena sudah di deklarasi diatas. kalau ingin mengganti nilainya langsung diketik age = 20.
+
+
+if condition
+percabangan / kondisi
+untuk if, akan return value boolean (true/false)
+
+switch case
+
+    
